ff7f54732168b22494b5f0026824cb85aa2b6972
Etienne Bouron <etienne.toutbien@gmail.com>
2017-10-25 13:44:42 +0200

    with functions create edit delete show

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,49 @@
+class GossipsController < ApplicationController
+
+	def index
+		@gossips = Gossip.all 
+	end
+
+	def new
+	end
+
+	def create
+		@gossip = Gossip.new(gossip_params)
+		if @gossip.save
+		redirect_to @gossip
+		else
+			render "new"
+		end
+	end
+
+	def edit
+		@gossip = Gossip.find(params[:id])
+	end
+
+	def destroy
+		@gossip = Gossip.find(params[:id])
+		@gossip.destroy 
+			redirect_to gossips_path
+	end
+ 
+	def update
+		@gossip = Gossip.find(params[:id])
+		if @gossip.update(gossip_params)
+			redirect_to @gossip
+		else
+			render "edit"
+		end
+	end
+
+	def show
+		@gossip = Gossip.find(params[:id])
+		puts "Ceci est mon Gossip : #{@gossip}"
+	end
+
+	private
+
+	def gossip_params
+		params.permit(:anonymous_author, :content)
+	end
+
+end
